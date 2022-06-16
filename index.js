bfe4969459b72ff087d66a2725a8702b0d5fbfb2
Fox Flaherty <katyra14@gmail.com>
2022-06-16 00:25:25 -0700

    not sure if I have the .env call back set up right

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+//get the enviroment variables
+require('.env').config()
+
+// require needed node modules
 const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
     res.send('Connected to Foxnet Successfully!')
 })
+
+// Listen to a port number defined by a local enviroment variable (.env)
+
+app.listen(process.env.PORT)
