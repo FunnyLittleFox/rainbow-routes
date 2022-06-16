5743000f732903f99d24bb3b36e68eed829e34ff
Fox Flaherty <katyra14@gmail.com>
2022-06-16 09:29:26 -0700

    Mostly all working and understand concept.  Cannot get the myColor to work or the .env still

--- index.js
+++ index.js
@@ -16,30 +16,50 @@ app.get('/', function (req, res) {
         </body>
         `)
 })
+// can't get the myColor to work?  Ask later.
 
-app.get('/teal', function (req, res) {
+app.get('/:color', function (req, res) {
+    let myColor = req.params.color
     res.send(`
     <body style="margin: 0;">
-            <div style="border: 1px solid black; height: 10vh; background-color: teal;">
+            <div style="border: 1px solid black; height: 10vh; background-color: $(myColor);">
                 <h2 style="text-align: center;">NAV BAR</h2>
             </div>
-        <h1 style="color: teal;">Teal Page</h1>
+        <h1 style="color: $(myColor);">$(myColor) Page</h1>
     </body>
     `)
 })
 
-app.get('/crimson', function (req, res) {
-    res.send(`
-    <body style="margin: 0;">
-        <div style="border: 1px solid black; height: 10vh; background-color: crimson;">
-            <h2 style="text-align: center;">NAV BAR</h2>
-        </div>
-        <h1 style="color: crimson;">Crimson Page</h1>
-    </body>
-    `)
-})
+
+//app.get('/teal', function (req, res) {
+    //res.send(`
+    //<body style="margin: 0;">
+            //<div style="border: 1px solid black; height: 10vh; background-color: teal;">
+                //<h2 style="text-align: center;">NAV BAR</h2>
+           // </div>
+        //<h1 style="color: teal;">Teal Page</h1>
+    //</body>
+   // `)
+//})
+
+// app.get('/crimson', function (req, res) {
+    //res.send(`
+    //<body style="margin: 0;">
+        //<div style="border: 1px solid black; height: 10vh; background-color: crimson;">
+           // <h2 style="text-align: center;">NAV BAR</h2>
+        //</div>
+        //<h1 style="color: crimson;">Crimson Page</h1>
+   // </body>
+   // `)
+//})
 
 // Listen to a port number defined by a local enviroment variable (.env)
+// could not get the .env to work.  ask about later
+
+//app.listen(process.env.PORT, function () {
+//   console.log('Connected to Foxnet')
+//})
+
 app.listen(3000, function () {
     console.log('Connected to Foxnet')
 })
